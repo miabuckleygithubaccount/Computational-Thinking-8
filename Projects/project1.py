56788ebe974f1ca8ed64c46976f2f917cf2254f4
miabuckleygithubaccount <miabuckley@seattleacademy.org>
2024-09-24 17:11:26 +0000

    project 1 submission

--- Projects/project1.py
+++ Projects/project1.py
@@ -13,12 +13,13 @@ q3 = codesters.Square(-100, -100, 200, 'LightSkyBlue')
 q4 = codesters.Square(100, -100, 200, 'DeepSkyBlue')
 
 
-s1 = codesters.Sprite("jumpingspooder", 100, 100)
+s1 = codesters.Sprite("spider", 100, 100)
 s2 = codesters.Sprite("thomas", -100, -100)
 s2.set_size(0.5)
-s3 = codesters.Sprite("drpepper" 100, -100)
+s3 = codesters.Sprite("dr pepper", 100, -100)
 s3.set_size(0.5)
-s4 = codesters.Sprite("cardinal", -100, 100)
+s4 = codesters.Sprite("wing stop", -100, 100)
 s4.set_size(0.1)
 
-message1 = codesters.Text("I love dr. pepper!", 0, 220, "navy")
+message1 = codesters.Text("I love dr. pepper!", 0, 220, "red")
+message2 = codesters.Text("Yay!", 0, -220, "black")
