95f8b2c1188fe4e778b90b46ff4e28bbc5612e60
miabuckleygithubaccount <miabuckley@seattleacademy.org>
2024-10-01 02:02:23 +0000

    project 2

--- Projects/project2.py
+++ Projects/project2.py
@@ -23,3 +23,35 @@ else:
 
 # choice 3
 c3 = input("Do you decide to go down the [hallway] or into the [bathroom]?")
+if "hallway" in c3:
+    print("You go down the hallway and find an old table and a key.")
+
+else:
+    print("You go into the bathroom and the clowns dog finds you and eats you.")
+
+# choice 4
+print("You grab the key and there's a room on your right and your left.")
+c4 = input("Do you go [right] or [left]?")
+if "right" in c4:
+    print("You walk right and into the dark room.")
+
+else:
+    print("You go left into the other room and the door shuts and locks behind you. You're stuck inside and you starve.")
+
+# choice 5
+print("In the room, you can take a [bat], a [crowbar] or a [sword].")
+c5 = input("What do you take for protection?")
+if "bat" in c5:
+    print("You pick up the bat.")
+elif "crowbar" in c5:
+    print("You take the crowbar.")
+else:
+    print("You take the sword just to find out it was just a prop and not real.")
+
+# choice 6
+print("You go downstairs, your weapon raised and ready. You find the clown.")
+c6 = input("Do you [hit] the clown on the head or [whack] him in the legs?")
+if "hit" in c6:
+    print("You hit him in the head and knock him out. You take the key you found and escape.")
+else:
+    print("You hit him in the legs and he falls, but then stands up and attacks you.")
---
+++ Projects/project3.py
@@ -0,0 +1,14 @@
+#Start
+import turtle 
+
+color= ["pink","violet","gray","lightgray"]
+for i in range (5)
+    t.color( colors[i % 4])
+    t.forward( distance)
+    t.left( angle ) 
+
+
+
+
+
+t = turtle.Turtle()
