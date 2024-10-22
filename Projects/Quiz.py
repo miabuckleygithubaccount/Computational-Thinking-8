d2d546d8367d4bc3fdfda2bf9055d9ad6fe5a834
miabuckleygithubaccount <miabuckley@seattleacademy.org>
2024-10-22 02:06:14 +0000

    project 4

---
+++ Projects/Quiz.py
@@ -0,0 +1,50 @@
+cat_points = 0
+dog_points = 0
+bunny_points = 0 
+
+# question 1:
+answer = input("After school do you A) take a nap, B) do homework, C) or do something you enjoy?")
+if answer == "A":
+	cat_points += 1
+elif answer == "B":
+	dog_points += 1
+elif answer == "C":
+	bunny_points += 1 
+
+# question 2:
+answer = input("Do you like A) reading B) watching a show, C) or playing games?")
+if answer == "A":
+	dog_points += 1,
+elif answer == "B":
+	cat_points += 1
+elif answer == "C":
+	bunny_points += 1
+
+# question 3:
+answer = input("Would you rather eat A) sushi for dinner, B) pizza, C) or tacos?")
+if answer == "A":
+	cat_points += 1
+elif answer == "B":
+	dog_points += 1
+elif answer == "C":
+	bunny_points += 1 
+
+# question 4:
+answer = input("Would you rather watch A) mean girls, B) mamma mia, C) or beauty and the beast?")
+if answer == "A":
+	cat_points += 1
+elif answer == "B":
+	dog_points += 1
+elif answer == "C":
+	bunny_points += 1 
+
+# question 5:
+answer = input("Would you rather bake A) cookies, B) muffins, C) or pumpkin bread?")
+if answer == "A":
+	cat_points += 1
+elif answer == "B":
+	dog_points += 1
+elif answer == "C":
+	bunny_points += 1 
+
+ 
